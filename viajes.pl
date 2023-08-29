46f2d4d777f0ab48ab662f31a25a446e9e02c8f3
Ezequiel Leonel Medrano Mamani <medranoezequiel07@gmail.com>
2023-08-28 21:56:20 -0300

    previaje

--- viajes.pl
+++ viajes.pl
@@ -1,28 +1,58 @@
-% Viajes
 
-% Tipos de transporte
+comercioAdherido(iguazu, grandHotelIguazu).%p para excursion
+comercioAdherido(iguazu, gargantaDelDiabloTour).
+comercioAdherido(bariloche, aerolineas).
+comercioAdherido(iguazu, aerolineas).
 
-viajes(charly,avion).  % no defini que charly no viaja en tren, porque eso no me aporta informacion sobre en que viaja charly.
-viajes(vilmaPalma,auto). % no le agregue vilmaPalma ni el color ni la hora porque no es algo que se use despues.
-viajes(arbol,trenes). % arbol tiene 3 tipos de transportes , y los defini en hechos distintos.
-viajes(arbol,camiones).
-viajes(arbol,tractor).
-viajes(zapatoVeloz,tractor). % no defini el color del tractor, porque no es algo que se use despues.
-%viajes(indio,auto). como no tiene a donde ir , entonces por concepto de universo cerrado, todo lo desconocido es falso
-%viajes(manuChao,_).como no se marca en que transporte va  , entonces por concepto de universo cerrado, todo lo desconocido es falso
-viajes(virus,taxi). % basicamente defini el transporte como lo marcaba el enunciado, obviando 'hotel Savoy y bailamos' porque no es algo que
-%se use despues.
+factura(estanislao, hotel(grandHotelIguazu, 2000)).
+factura(antonieta, excursion(gargantaDelDiabloTour, 5000, 4)).
+factura(antonieta, vuelo(1515, antonietaPerez)).
 
+valorMaximoHotel(5000).
 
-% Chetos
-
-chetosViajando(PersonaNueva):-viajes(PersonaNueva,taxi).
-chetosViajando(PersonaNueva):-viajes(PersonaNueva,avion).
+% los vuelos que se hicieron.
+registroVuelo(1515, iguazu, aerolineas, 
+  [estanislaoGarcia,antonietaPerez, danielIto]
+  , 10000).
 
 
-%Gustos similares
+%TIPO DE FACTURA = HOTEL O EXCURSION O VUELO
+montoADevolver(Persona,Monto):-
+  factura(Persona,TipoDeFactura),
+  facturaValida(TipoDeFactura), 
+  findall(MontoDeLaFactura,(montoPagado(Persona,MontoDeLaFactura),ciudadesDistintasEnLasQueEstuvo(Persona,MontoDeLaFactura))
+  ,sum_list(MontoDeLaFactura, Monto)).
 
-personasDistintas(Persona1,Persona2):-Persona1 \= Persona2.
+montoADevolver(Persona,Monto):-
+  factura(Persona,TipoDeFactura),
+  not(facturaValida(TipoDeFactura)),
+  Monto is Monto - 15000.
+  
 
-tienenGustosSimilares(Persona1,Persona2):-personasDistintas(Persona1,Persona2),viajes(Persona1,Transporte),
-viajes(Persona2,Transporte).
+montoPagado(hotel(_,Total)).
+montoPagado(excursion(_,Total,_)).
+montoPagado(vuelo(IdDeVuelo,_),TotalDeVuelo):-
+  (registroVuelo(IdDeVuelo,_,_,[],Total)),
+  TotalDeVuelo is Total.
+
+
+
+ciudadesDistintasEnLasQueEstuvo(hotel(Sitio,_),MontoDeLaFactura):-
+  findall(Ciudad,comercioAdherido(Ciudad,Sitio),Ciudades),
+  length(Ciudades,Total),
+  MontoDeLaFactura is Total * 1000.
+
+ciudadesDistintasEnLasQueEstuvo(excursion(Sitio,_,_),MontoDeLaFactura):-
+  findall(Ciudad,comercioAdherido(Ciudad,Sitio),Ciudades),
+  length(Ciudades,Total),
+  MontoDeLaFactura is Total * 1000.
+
+facturaValida(hotel(grandHotelIguazu(Precio))):-
+  Precio =< 5000.
+facturaValida(vuelo(_,NombreCompleto)):-
+  length(NombreCompleto, Valor),
+  Valor > 9.
+facturaValida(excursion(NombreDeLaExcursion,_,_)):-
+  comercioAdherido(_,NombreDeLaExcursion).
+  
+
