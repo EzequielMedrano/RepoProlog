bf5f657a831436b350891aed9525754ed22f574a
Ezequiel Leonel Medrano Mamani <medranoezequiel07@gmail.com>
2023-08-29 20:30:33 -0300

    ParcialAsadito

--- viajes.pl
+++ viajes.pl
@@ -1,28 +1,134 @@
-% Viajes
+% define quiénes son amigos de nuestro cliente
+amigo(mati). 
+amigo(pablo).
+amigo(leo).
+amigo(fer).
+amigo(flor).
+amigo(ezequiel).
+amigo(marina).
 
-% Tipos de transporte
+% define quiénes no se pueden ver
+noSeBanca(leo, flor).
+noSeBanca(pablo, fer).
+noSeBanca(fer, leo). 
+noSeBanca(flor, fer).
 
-viajes(charly,avion).  % no defini que charly no viaja en tren, porque eso no me aporta informacion sobre en que viaja charly.
-viajes(vilmaPalma,auto). % no le agregue vilmaPalma ni el color ni la hora porque no es algo que se use despues.
-viajes(arbol,trenes). % arbol tiene 3 tipos de transportes , y los defini en hechos distintos.
-viajes(arbol,camiones).
-viajes(arbol,tractor).
-viajes(zapatoVeloz,tractor). % no defini el color del tractor, porque no es algo que se use despues.
-%viajes(indio,auto). como no tiene a donde ir , entonces por concepto de universo cerrado, todo lo desconocido es falso
-%viajes(manuChao,_).como no se marca en que transporte va  , entonces por concepto de universo cerrado, todo lo desconocido es falso
-viajes(virus,taxi). % basicamente defini el transporte como lo marcaba el enunciado, obviando 'hotel Savoy y bailamos' porque no es algo que
-%se use despues.
+% define cuáles son las comidas y cómo se componen
+% functor achura contiene nombre, cantidad de calorías
+% functor ensalada contiene nombre, lista de ingredientes
+% functor morfi contiene nombre (el morfi es una comida principal)
+comida(achura(chori, 200)). % ya sabemos que el chori no es achura
+comida(achura(chinchu, 150)).
+comida(ensalada(waldorf, [manzana, apio, nuez, mayo])).
+comida(ensalada(mixta, [lechuga, tomate, cebolla])).
+comida(morfi(vacio)).
+comida(morfi(mondiola)).
+comida(morfi(asado)).
+
+% relacionamos la comida que se sirvió en cada asado
+% cada asado se realizó en una única fecha posible: functor fecha + comida
+asado(fecha(22,9,2011), chori).
+asado(fecha(15,9,2011), mixta).
+asado(fecha(22,9,2011), waldorf).
+asado(fecha(15,9,2011), mondiola).
+asado(fecha(22,9,2011), vacio).
+asado(fecha(15,9,2011), chinchu).
+% relacionamos quiénes asistieron a ese asado
+asistio(fecha(15,9,2011), flor). 
+asistio(fecha(22,9,2011), marina).
+asistio(fecha(15,9,2011), pablo).
+asistio(fecha(22,9,2011), pablo).
+asistio(fecha(15,9,2011), leo).
+asistio(fecha(22,9,2011), flor).
+asistio(fecha(15,9,2011), fer).
+asistio(fecha(22,9,2011), mati).
+% definimos qué le gusta a cada persona
+leGusta(mati, chori). 
+leGusta(fer, mondiola).
+leGusta(pablo, asado).
+leGusta(mati, vacio).
+leGusta(fer, vacio).
+leGusta(mati, waldorf).
+leGusta(flor, mixta).
+
+% a. A Ezequiel le gusta lo que le gusta a Mati y a Fer.
+% b. A Marina le gusta todo lo que le gusta a Flor y la mondiola.
+% c. A Leo no le gusta la ensalada waldorf.
+
+leGusta(ezequiel,Comida):-
+  leGusta(mati,Comida),
+  leGusta(fer,Comida).
+
+leGusta(marina,Comida):-
+  leGusta(flor,Comida).
+
+leleGusta(marina,mondiola).
+
+asadoViolento(FechaDelAsado):-
+  asistio(FechaDelAsado,Persona),
+  % noSeBanca(Persona,PersonaQueNoBanca),
+  forall(asistio(FechaDelAsado,Persona),noSeBanca(Persona,_)).
+  
+
+%%%3) Definir el predicado calorías/2 que relaciona las calorías de una comida
+% a. Las ensaladas tienen una caloría por ingrediente (la mixta, por ejemplo, tiene 3 calorías)
+% b. Las achuras definen su propias calorías (el chori tiene 200 calorías)
+% c. El morfi tiene siempre 200 calorías, no importa de qué morfi se trate
 
 
-% Chetos
+calorias(Comida,Calorias):-
+  comida(achura(Comida,Calorias)).
 
-chetosViajando(PersonaNueva):-viajes(PersonaNueva,taxi).
-chetosViajando(PersonaNueva):-viajes(PersonaNueva,avion).
+calorias(Comida,Calorias):-
+  comida(ensalada(Comida,Ingredientes)),
+  length(Ingredientes, Calorias).
 
+calorias(Comida,200):-
+    comida((morfi(Comida))).
 
-%Gustos similares
+asadoFlojito(FechaDeAsado):-
+  % asado(FechaDeAsado,Comida),
+  asado(FechaDeAsado,_),
+  findall(Caloria,(asado(FechaDeAsado,Comida),calorias(Comida,Caloria)),Calorias),
+  sum_list(Calorias,Calor),
+  Calor<400.
+ %%%%%%%%%%%%% PUNTO 5 %%%%%%%%%%%%%%%%%%%%
+hablo(fecha(15,09,2011), flor, pablo).
+hablo(fecha(22,09,2011), flor, marina).
+hablo(fecha(15,09,2011), pablo, leo).
+hablo(fecha(22,09,2011), marina, pablo).
+hablo(fecha(15,09,2011), leo, fer).
+reservado(marina).
 
-personasDistintas(Persona1,Persona2):-Persona1 \= Persona2.
+chismeDe(FechaDeAsado,PersonaQueCuenta,PersonaQueEscucha):-
+      hablo(FechaDeAsado,PersonaQueCuenta,PersonaQueQuiereEscuchar),
+      not(PersonaQueQuiereEscuchar\= PersonaQueEscucha),
+      findall(PersonasQueEscuchan,hablo(FechaDeAsado,PersonaQueCuenta,PersonasQueEscuchan),PersonasEscuchando),
+      cadenaDeChismesDe(PersonaQueQuiereEscuchar,PersonasEscuchando).
 
-tienenGustosSimilares(Persona1,Persona2):-personasDistintas(Persona1,Persona2),viajes(Persona1,Transporte),
-viajes(Persona2,Transporte).
+cadenaDeChismesDe(PersonaQueQuiereEscuchar , Persona|Personas):-
+  
+   
+ %%%%%%%%%%%%% PUNTO 6 %%%%%%%%%%%%%%%%%%%%
+
+disfruto(Persona,FechaDeAsado):-
+  asistio(FechaDeAsado,Persona), %%% esto lo uso porque necesito unificarlas antes de meterlo al findall.
+    findall(Comida,(leGusta(Persona,Comida),asistio(FechaDeAsado,Persona)),CantidadDeComidaQueLeGusto),
+   length(CantidadDeComidaQueLeGusto,TotalidadDeComida),
+   TotalidadDeComida >= 3. 
+
+ %%%%%%%%%%%%% PUNTO 7 %%%%%%%%%%%%%%%%%%%%
+asadoRico(Comida):-
+  comida(Comida),
+  forall(comida(Comida),esUnaComidaRica(Comida)).
+
+esUnaComidaRica(morfi(_)). %%comida(morfi(vacio)).
+
+esUnaComidaRica(ensalada(_,Ingredientes)):-  %% Ingredientes es una lista , pero no necesito ponerlo en una lista
+ length(Ingredientes,TotalDeIngredientes),
+ TotalDeIngredientes > 3.
+
+ %%comida(achura(chori, 200)).
+esUnaComidaRica(achura(chori,_)).
+esUnaComidaRica(achura(morci,_)).
+ %%con esto descarto todos los tipos de comida que no existen mi universo.
